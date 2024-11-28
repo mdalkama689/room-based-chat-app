e7c57086e2db553a51df0039b65c00bf296fa7f7
mdalkama <mdalkama689@gmail.com>
2024-11-28 14:03:34 +0530

    add logic to join the room

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,42 @@
-console.log("start")
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+// message
+// {
+//     "type": "join",
+// "payload": {
+// roomId: "1"
+// }
+
+// }
+
+const allSockets: any = [];
+
+wss.on("connection", (socket) => {
+  console.log("new user connected");
+
+  socket.on("message", (message) => {
+    const parseMessage = JSON.parse(message.toString());
+    console.log(" parseMessage : ", parseMessage);
+
+    if (parseMessage.type === "join") {
+      socket.send(`You joined room ${parseMessage.payload.roomId}`);
+      const room = {
+        socket,
+        message: parseMessage,
+      };
+
+      allSockets.push(room);
+      allSockets.forEach((user: any) => {
+        if (user?.message?.payload?.roomId === parseMessage.payload.roomId &&
+            user.socket != socket
+
+        ) {
+          user?.socket?.send(`new user coonected to room : ${parseMessage.payload.roomId}`);
+          console.log("send message to al user in the room");
+        }
+      });
+    }
+  });
+});
